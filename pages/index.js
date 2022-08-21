4fda58a1c9f82f84f24175ef84b61077d526bdaf
kiseki-kiseki <kcanggua@gmail.com>
2022-08-21 18:44:52 +0900

    change io

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 import React, { useState } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://nextchatback.herokuapp.com/");
 
 export default function Home() {
   const [message, setMessage] = useState("");
